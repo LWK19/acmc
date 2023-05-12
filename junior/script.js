96d7a347e12da4b9228a733a455c803b2157f1fb
Lee Wai Kin <leewaikin19@gmail.com>
2023-05-12 12:46:37 +0800

    a

--- junior/script.js
+++ junior/script.js
@@ -2,6 +2,7 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 globalThis.qn;
+globalThis.time;
 ////////////////////////////////////////////////////////////////
 //API.js
 
