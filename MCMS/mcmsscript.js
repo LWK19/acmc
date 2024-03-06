183d56621e5b4a282c565131f3c4e0326c6836f9
Dominic Bryan <dominicbryangunawan@gmail.com>
2024-03-06 14:56:18 +0800

    mmm

--- MCMS/mcmsscript.js
+++ MCMS/mcmsscript.js
@@ -20,11 +20,17 @@ function addNewInputRow(table, rowindex = -1) {
     var cells = [];
     for (var i = 0; i < numcol; i++) {
         cells.push(row.insertCell(i));
-        cells[i].innerHTML = '<input class="participantinput" onkeypress="rowhandler(event)">';
+        cells[i].innerHTML = '<input class="participantinput" onkeyup="rowhandler(event)">';
     }
     cells[0].getElementsByTagName('input')[0].focus();
 }
 
-function deleteInputRow(table, rowindex) {
-    table.deleteRow(rowindex - 1);
+function deleteInputRow(table, rowindex = table.rows.length - 1) {
+    if (Array.from(table.rows[rowindex].cells).every(cell => cell.children[0].value === "")) {
+        if (rowindex > 1) {
+            table.deleteRow(rowindex);
+        }
+    }
+    var numcol = table.rows[0].cells.length;
+    table.rows[rowindex-1].cells[numcol - 1].children[0].focus();
 }
